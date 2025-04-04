d170ba7fc3ff688709db77086aa2e8f78e4597b2
Felicitas <felicitas.aguerralde@gmail.com>
2025-04-04 10:17:07 -0300

    Pego html de maquetacio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+        <header className="header">
+            <h1>Popcorn film</h1>
+            <img src="img/popcorn.svg" alt="Logo de Popcorn Film" />
+        </header>
+        <nav className="nav">
+            <ul>
+                <li><a href="/#">Inicio</a></li>
+                <li><a href="/#">Películas</a></li>
+                <li><a href="/#">Blog</a></li>
+                <li><a href="/#">Contacto</a></li>
+            </ul>
+        </nav>
+
+        <section className="content">
+        <div className="featured">
+            <article className="item">
+                <h3 className="title">Desarrollo Web</h3>
+                <p className="description">Historia de la WEB</p>
+                <button className="edit">Editar</button>
+                <button className="delete">Eliminar</button>
+            </article>
+
+            <article className="item">
+                <h3 className="title">Desarrollo Web</h3>
+                <p className="description">Historia de la WEB</p>
+                <button className="edit">Editar</button>
+                <button className="delete">Eliminar</button>
+            </article>
+
+            <article className="item">
+                <h3 className="title">Desarrollo Web</h3>
+                <p className="description">Historia de la WEB</p>
+                <button className="edit">Editar</button>
+                <button className="delete">Eliminar</button>
+            </article>
+
+            <article className="item">
+                <h3 className="title">Desarrollo Web</h3>
+                <p className="description">Historia de la WEB</p>
+                <button className="edit">Editar</button>
+                <button className="delete">Eliminar</button>
+            </article>
+
+            <article className="item">
+                <h3 className="title">Desarrollo Web</h3>
+                <p className="description">Historia de la WEB</p>
+                <button className="edit">Editar</button>
+                <button className="delete">Eliminar</button>
+            </article>
+            
+            <article className="item">
+                <h3 className="title">Desarrollo Web</h3>
+                <p className="description">Historia de la WEB</p>
+                <button className="edit">Editar</button>
+                <button className="delete">Eliminar</button>
+            </article>
+
+            <article className="item">
+                <h3 className="title">Desarrollo Web</h3>
+                <p className="description">Historia de la WEB</p>
+                <button className="edit">Editar</button>
+                <button className="delete">Eliminar</button>
+            </article>
+
+            <article className="item">
+                <h3 className="title">Desarrollo Web</h3>
+                <p className="description">Historia de la WEB</p>
+                <button className="edit">Editar</button>
+                <button className="delete">Eliminar</button>
+            </article>
+            </div>
+        </section>
+
+        <aside className="lateral">
+            <div className="search">
+                <h3 className="title">Buscador</h3>
+                <form>
+                    <input type="text" placeholder="Buscar..." />
+                    <button type="submit">Buscar</button>
+                </form>
+            </div>
+
+            <div className="add">
+                <h3 className="title">Agregar Pelicula</h3>
+                <form>
+                    <input type="text" placeholder="Nombre de la pelicula" />
+                    <textarea placeholder="Descripción"></textarea>
+                    <button type="submit">Agregar</button>
+                </form>
+            </div>
+        </aside>
+        <footer className="footer">
+            <p>&copy; 2025 Proyecto de Películas. Todos los derechos reservados.</p>
+        </footer>
     </div>
   );
 }
